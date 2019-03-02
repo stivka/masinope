c22a0a29d9419afe00c3cad211578dcb384d6920
Stiv Kapten <stkapt@ttu.ee>
2019-03-02 23:54:56 +0200

    counting conflicts, but error on counting conflicts

--- lab3 -/hill_climbing.py
+++ lab3 -/hill_climbing.py
@@ -22,7 +22,65 @@ class Queen:
 
     # calculate no. of conflicts
     def calculateValue(self, board):
-        print("Queen " + str(self.col) + " on row " + str(self.row))
+        #print("Queen " + str(self.col) + " on row " + str(self.row))
+        matrix = board.matrix
+        printMatrix(matrix)
+        conflictOnCol = False
+        conflictOnRow = False
+        conflictOnDiag = False
+        conflictOnCDiag = False
+
+        for i in range (0, board.N):
+            # checks whole column. If isn't itself and is a Queen
+            if(i != self.row and matrix[self.col][i] == "Q" and not conflictOnCol):
+                self.value += 1
+                conflictOnCol= True
+                print("Queen " + str(self.col) + " has conflict on column")
+
+            # check row
+            if(i != self.col and matrix[i][self.row] == "Q") and not conflictOnRow:
+                self.value += 1
+                conflictOnRow = True
+                print("Queen " + str(self.col) + " has conflict on row")
+
+        for i in range (1, board.N):
+            # check diagonal down
+            new_col = self.col + i
+            new_row = self.row + i
+            if (new_col < board.N and new_row < board.N):
+                if(matrix[new_col][new_row] == "Q" and not conflictOnDiag):
+                    self.value += 1
+                    conflictOnDiag = True
+                    print("Queen " + str(self.col) + " has conflict on diagonal")
+            # check diagonal up
+            new_col = self.col - i
+            new_row = self.row - i
+            if (new_col >= 0 and new_row >= 0):
+                if(matrix[new_col][new_row] == "Q" and not conflictOnDiag):
+                    self.value += 1
+                    conflictOnDiag = True
+                    print("Queen " + str(self.col) + " has conflict on diagonal")
+            
+            # check counter-diagonal down
+            new_col = self.col + i
+            new_row = self.row - i
+            if (new_col < board.N and new_row >= 0):
+                if(matrix[new_col][new_row] == "Q" and not conflictOnCDiag):
+                    self.value += 1
+                    conflictOnCDiag = True
+                    print("Queen " + str(self.col) + " has conflict on counter-diagonal")
+
+            # check counter-diagonal up
+            new_col = self.col - i
+            new_row = self.row + i
+            if (new_col >= 0 and new_row < board.N):
+                if(matrix[new_col][new_row] == "Q" and not conflictOnCDiag):
+                    self.value += 1
+                    conflictOnCDiag = True
+                    print("Queen " + str(self.col) + " has conflict on counter-diagonal")
+
+
+        print("Queen " + str(self.col) + " has " + str(self.value) + " conflict(s).")
 
     def best_move(self):
         # find the best move and the value function after making that move
@@ -45,15 +103,6 @@ class Board:
     def makeMove(self, move):
         # actually execute a move (change the board)
         return 0
-
-    def getArray(self):
-        return self.array
-
-    def getQueens(self):
-        return self.queens
-    
-    def getQueen(self, col):
-        return self.queens[col]
 
     def createArray(self):
         for i in range(0, self.N):
@@ -80,12 +129,18 @@ class Board:
                 print(self.matrix[col][row], end=" ")
             print(" ")
 
+def printMatrix(matrix):
+        for col in range(0, len(matrix)):
+            for row in range(0, len(matrix[0])):
+                print(matrix[col][row], end=" ")
+            print(" ")
 
 # create 4x4 board
 board = Board(4)
-board.printMatrix()
+#board.printMatrix()
 
-for i in range(len(board.queens)):
-    board.queens[i].calculateValue(board)
-    #board.getQueen(i).calculateValue
-    #hill_climbing(q)
+#for i in range(len(board.queens)):
+    #board.queens[i].calculateValue(board)
+board.queens[1].calculateValue(board)
+    # board.getQueen(i).calculateValue
+    # hill_climbing(q)
